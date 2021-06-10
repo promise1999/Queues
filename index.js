1e1f8cd6012b0fde41edcff80378769f9de46d1c
Mike Ryan <mike.ryan@keyin.com>
2021-06-10 16:27:59 -0230

    minor updates

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.listen(port, () => {
 })
 
 /*
+
 CRUD -create (save, post) Retrieve (get, fetch), Update modify, put), Delete (remove, purge, prune)
 
 problem 1 - create a service with 2 end points -> save and retrieve (stack) LIFO
@@ -28,4 +29,6 @@ psuedocode -
 app.post /xyz (save) -
 |_ stack (push)
     |_database insert into postgres
-app.get /xyz (retrieve) - stack () */
+app.get /xyz (retrieve) - stack () 
+
+*/
